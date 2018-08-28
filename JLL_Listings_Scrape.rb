9d14382e610b5bb633dfc542eaa09f4351ae9609
Jmwofford <jmwofford@gmail.com>
2018-08-28 04:07:40 -0500

    Finding JLL_Data to curl thenConvert

--- JLL_Listings_Scrape.rb
+++ JLL_Listings_Scrape.rb
@@ -1,3 +1,16 @@
 module DataSourcing
+# module that organizes the functionality of the data thats being sourced
+
+	module Jll
+	    # organizes company (CBRE) that will hold all pertinent information for listings and properties- CBRE_info
+class Prop_Listing_Scraper
+
+
+
+
+
+		end
+
+	end
 
 end
