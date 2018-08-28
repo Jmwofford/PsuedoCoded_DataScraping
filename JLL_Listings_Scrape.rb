9e4dace080976e1392d3aa2be1a5645f5077c9ef
Jmwofford <jmwofford@gmail.com>
2018-08-28 04:30:26 -0500

    found Request..curled then converted..Looking to retrieve and parse jSON, will test in irb

--- JLL_Listings_Scrape.rb
+++ JLL_Listings_Scrape.rb
@@ -3,9 +3,26 @@ module DataSourcing
 
 	module Jll
 	    # organizes company (CBRE) that will hold all pertinent information for listings and properties- CBRE_info
-class Prop_Listing_Scraper
+		class Prop_Listing_Scraper
 
+		uri = URI.parse("https://gewerbeimmobilien.jll.de/ergebnis/germany/gewerbe-einzelhandel/tenuretype-miete/")
+		request = Net::HTTP::Get.new(uri)
+		request["Upgrade-Insecure-Requests"] = "1"
+		request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+		request["Dnt"] = "1"
+		request["X-Devtools-Emulate-Network-Conditions-Client-Id"] = "524AD8F9BCE7C613C7D81EFBF7025B0D"
+		request["Referer"] = "https://gewerbeimmobilien.jll.de/einzelhandel/e0573/"
 
+		req_options = {
+		  use_ssl: uri.scheme == "https",
+		}
+
+		response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+		  http.request(request)
+		end
+		body = response.body
+		# bulk of returned data for request
+		
 
 
 
